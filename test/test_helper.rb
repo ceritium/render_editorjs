1a9a1540eb1d2c054a49dcd1a6ed50584a545a49
José Galisteo <ceritium@gmail.com>
2021-11-07 23:49:15 +0100

    formatter

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,6 +1,13 @@
 # frozen_string_literal: true
 
+require 'codeclimate-test-reporter'
 require "simplecov"
+
+SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new([
+  SimpleCov::Formatter::HTMLFormatter,
+  CodeClimate::TestReporter::Formatter
+])
+
 SimpleCov.start do
   add_filter "/test/"
 end
